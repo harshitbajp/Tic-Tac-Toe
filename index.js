39b32cc28eaf552294cb1dc6383cce80cc6483f3
harshitbajp <harshit.bajpai.77777@gmail.com>
2023-08-24 21:40:29 +0530

    first_draft

---
+++ index.js
@@ -0,0 +1,117 @@
+const boxes = document.querySelectorAll(".box");
+const gameInfo = document.querySelector(".game-info");
+const newGameBtn = document.querySelector(".btn");
+
+let currentPlayer;
+let gameGrid;
+
+const winningPositions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+//lets create a func to initialise the game
+
+function initGame() {
+    currentPlayer = "X";
+    gameGrid = ["","","","","","","","",""];
+    //UI pr empty bhi karna padega boxes ko
+    boxes.forEach((box, index) => {
+        box.innerText = "";
+        boxes[index].style.pointerEvents = "all";
+        //one more thing is missing, initialise box with css properties again
+        box.classList = `box box${index+1}`;
+    });
+    newGameBtn.classList.remove("active");
+    gameInfo.innerText = `Current Player - ${currentPlayer}`;
+}
+
+
+initGame();
+
+boxes.forEach((box,index)=>{
+    box.addEventListener("click",()=> {
+        handleClick(index);
+    })
+});
+
+function handleClick(index){
+    if(gameGrid[index]===""){
+        boxes[index].innerHTML=currentPlayer;
+        gameGrid[index]=currentPlayer;
+        //swap karo turn ko
+        swapTurn();
+        checkGameOver();
+    }
+}
+
+function swapTurn(){
+    if(currentPlayer==="X"){
+        currentPlayer="0";
+    }
+
+    else{
+        currentPlayer="X";
+    }
+
+    gameInfo.innerText = `Current Player - ${currentPlayer}`;
+}
+
+newGameBtn.addEventListener("click",initGame);
+
+function checkGameOver(){
+    let answer = "";
+    
+    winningPositions.forEach((position) => {
+        //all 3 boxes should be non-empty and exactly same in value
+        if( (gameGrid[position[0]] !== "" || gameGrid[position[1]] !== "" || gameGrid[position[2]] !== "") 
+            && (gameGrid[position[0]] === gameGrid[position[1]] ) && (gameGrid[position[1]] === gameGrid[position[2]])) {
+
+                //check if winner is X
+                if(gameGrid[position[0]] === "X") 
+                    answer = "X";
+                else {
+                    answer = "O";
+                } 
+                    
+
+                //disable pointer events
+                boxes.forEach((box) => {
+                    box.style.pointerEvents = "none";
+                })
+
+                //now we know X/O is a winner
+                boxes[position[0]].classList.add("win");
+                boxes[position[1]].classList.add("win");
+                boxes[position[2]].classList.add("win");
+            }
+    });
+
+    //answer is non-empty -- means we have a winner
+    if(answer !== ""){
+        gameInfo.innerText=`Winner Player - ${answer}`;
+        newGameBtn.classList.add("active");
+        return;
+    }
+
+    //check for tie
+
+    let fillCount = 0;
+    gameGrid.forEach((box) => {
+        if(box !== "" )
+            fillCount++;
+    });
+
+    //board is Filled, game is TIE
+    if(fillCount === 9) {
+        gameInfo.innerText = "Game Tied !";
+        newGameBtn.classList.add("active");
+    }
+
+}
